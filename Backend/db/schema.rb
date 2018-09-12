ffd683dc47190b7c8a0a1c912ebc97dca5cb19b0
Jonathan Gonzalez <jonathan.gonzalez-b@outlook.com>
2018-09-11 21:32:19 -0500

    mapeo de base de datos relacional y diseño de frontend

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -12,124 +12,120 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "calificacion", primary_key: ["idCalificacion", "Pedido_idPedido"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "idCalificacion", null: false
-    t.integer "Puntaje"
-    t.string "Comentario", limit: 200
-    t.integer "Pedido_idPedido", null: false
-    t.index ["Pedido_idPedido"], name: "fk_Calificacion_Pedido1_idx"
+  create_table "accounts", primary_key: "id_account", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Guarda informacion bancaria para recibir/pagar las ordenes de compra" do |t|
+    t.integer "id_person", null: false
+    t.string "type_ID", limit: 3, null: false
+    t.string "type_account", limit: 45, comment: "Ahorro, Corriente, Credito"
+    t.integer "number"
+    t.string "name_bank", limit: 45
+    t.index ["id_person", "type_ID"], name: "fk_Account_Person1_idx"
   end
 
-  create_table "categoria", primary_key: "CategoriaId", id: :integer, default: nil, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "NombreCategoria", limit: 50
-    t.string "Descripcion", limit: 500
+  create_table "califications", primary_key: "id_calification", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Se registra el puntaje de cada compra hecha por el cliente" do |t|
+    t.integer "id_order", null: false
+    t.integer "score"
+    t.string "comment", limit: 200
+    t.index ["id_order"], name: "fk_Calificacion_Pedido1_idx"
   end
 
-  create_table "cliente", primary_key: ["TipoId", "IdUsuario"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "TipoId", limit: 3, null: false
-    t.integer "IdUsuario", null: false
-    t.string "TipoPersona", limit: 15, comment: "Juridica o Natrual"
-    t.string "Nit", limit: 20
-    t.string "Descripcion", limit: 45
-    t.string "Url", limit: 45
-    t.index ["TipoId", "IdUsuario"], name: "fk_Consumidor_Usuario1_idx"
+  create_table "categories", primary_key: "id_category", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Se indican las categorias de los productos hidroponicos" do |t|
+    t.string "name", limit: 50
+    t.string "description", limit: 500
   end
 
-  create_table "cuenta", primary_key: ["idCuenta", "TipoId", "IdUsuario"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Información bancaria para realizar/recibir el pago de los pedidos" do |t|
-    t.string "idCuenta", limit: 45, null: false
-    t.string "TipoCuenta", limit: 45, comment: "Ahorro, Corriente, Credito"
-    t.integer "NumeroCuenta"
-    t.string "NombreBanco", limit: 45
-    t.string "TipoId", limit: 3, null: false
-    t.integer "IdUsuario", null: false
-    t.index ["TipoId", "IdUsuario"], name: "fk_Cuenta_Usuario1_idx"
+  create_table "clients", primary_key: "id_client", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "En esta tabla se registran las personas como clientes" do |t|
+    t.integer "id_person", null: false
+    t.string "type_ID", limit: 3, null: false
+    t.string "kind_person", limit: 15, comment: "Juridica o Natrual"
+    t.string "nit", limit: 20
+    t.string "description", limit: 45
+    t.string "url", limit: 45
+    t.index ["id_person", "type_ID"], name: "fk_Client_Person1_idx"
   end
 
-  create_table "cultivador", primary_key: ["TipoId", "IdUsuario"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "TipoId", limit: 3, null: false
-    t.integer "IdUsuario", null: false
-    t.date "FechaNacimiento"
-    t.string "Genero", limit: 1
-    t.index ["TipoId", "IdUsuario"], name: "fk_Cultivador_Usuario1_idx"
+  create_table "cultivator_has_products", primary_key: "id_cultivator_has_products", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Esta tabla permite conocer cuales productos estan cultivando los cultivadores, ademas de saber su disponibilidad" do |t|
+    t.integer "id_person", null: false
+    t.string "type_ID", limit: 3, null: false
+    t.integer "id_product", null: false
+    t.integer "availability"
+    t.date "update_date", comment: "Tabla para actualizar la disponibilidad de cada producto"
+    t.index ["id_person", "type_ID"], name: "fk_Cultivator_has_Product_Cultivator1_idx"
+    t.index ["id_product"], name: "fk_Cultivator_has_Product_Product1_idx"
   end
 
-  create_table "cultivador_has_producto", primary_key: ["Cultivador_TipoId", "Cultivador_IdUsuario", "Producto_ProductoId"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "Cultivador_TipoId", limit: 3, null: false
-    t.integer "Cultivador_IdUsuario", null: false
-    t.integer "Producto_ProductoId", null: false
-    t.integer "Disponibilidad"
-    t.date "FechaActualizacion"
-    t.index ["Cultivador_TipoId", "Cultivador_IdUsuario"], name: "fk_Cultivador_has_Producto_Cultivador1_idx"
-    t.index ["Producto_ProductoId"], name: "fk_Cultivador_has_Producto_Producto1_idx"
+  create_table "cultivators", primary_key: "id_cultivator", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "En esta tabla se registran las personas como cultivadores" do |t|
+    t.integer "id_person", null: false
+    t.string "type_ID", limit: 3, null: false
+    t.date "birth_day"
+    t.string "gender", limit: 1
+    t.index ["id_person", "type_ID"], name: "fk_Cultivator_Person1"
   end
 
-  create_table "despacho", primary_key: ["Pedido_idPedido", "Cultivador_TipoId", "Cultivador_IdUsuario"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "Pedido_idPedido", null: false
-    t.string "Cultivador_TipoId", limit: 3, null: false
-    t.integer "Cultivador_IdUsuario", null: false
-    t.date "FechaEntrega"
-    t.index ["Cultivador_TipoId", "Cultivador_IdUsuario"], name: "fk_Despacho_Cultivador1_idx"
-    t.index ["Pedido_idPedido"], name: "fk_Despacho_Pedido1_idx"
+  create_table "dispatches", primary_key: "id_dispatch", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "se guardan los datos de quien fue el encargado de atender la orden de compra" do |t|
+    t.integer "id_person", null: false
+    t.string "type_ID", limit: 3, null: false
+    t.integer "id_order", null: false
+    t.date "dispatch_date"
+    t.index ["id_order"], name: "fk_Despacho_Pedido1_idx"
+    t.index ["id_person", "type_ID"], name: "fk_Dispatch_Cultivator1_idx"
   end
 
-  create_table "lineacompra", primary_key: ["Producto_ProductoId", "Pedido_idPedido"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "Cantidad"
-    t.decimal "Preciounitario", precision: 20
-    t.integer "Producto_ProductoId", null: false
-    t.integer "Pedido_idPedido", null: false
-    t.index ["Pedido_idPedido"], name: "fk_LineaCompra_Pedido1_idx"
-    t.index ["Producto_ProductoId"], name: "fk_LineaCompra_Producto1_idx"
+  create_table "order_has_products", primary_key: "id_order_has_products", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Permite relacionar que productos solicito el usuario al momento de realizar su cotizacion" do |t|
+    t.integer "id_product", null: false
+    t.integer "id_order", null: false
+    t.integer "quantity"
+    t.decimal "unitPrice", precision: 20
+    t.index ["id_order"], name: "fk_LineaCompra_Pedido1_idx"
+    t.index ["id_product"], name: "fk_LineaCompra_Producto1_idx"
   end
 
-  create_table "pedido", primary_key: ["idPedido", "Cliente_TipoId", "Cliente_IdUsuario"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "idPedido", null: false
-    t.decimal "Total", precision: 20
-    t.date "FechaSolicitud"
-    t.string "Cliente_TipoId", limit: 3, null: false
-    t.integer "Cliente_IdUsuario", null: false
-    t.index ["Cliente_TipoId", "Cliente_IdUsuario"], name: "fk_Pedido_Cliente1_idx"
+  create_table "orders", primary_key: "id_order", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Tabla donde se guarda la solicitud de compra por parte del cliente" do |t|
+    t.integer "id_person", null: false
+    t.string "type_ID", limit: 3, null: false
+    t.decimal "total", precision: 20
+    t.date "application_date"
+    t.index ["id_person", "type_ID"], name: "fk_Order_Client1_idx"
   end
 
-  create_table "producto", primary_key: ["ProductoId", "Categoria_CategoriaId"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "ProductoId", null: false
-    t.string "Nombre", limit: 50
-    t.string "Descripcion", limit: 500
-    t.decimal "Precio", precision: 10
-    t.binary "Imagen", limit: 4294967295
-    t.integer "Categoria_CategoriaId", null: false
-    t.index ["Categoria_CategoriaId"], name: "fk_Producto_Categoria1_idx"
+  create_table "products", primary_key: "id_product", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Tabla maestra para los productos existentes en el sistema" do |t|
+    t.integer "id_category", null: false
+    t.string "name", limit: 50
+    t.string "description", limit: 500
+    t.decimal "price", precision: 10
+    t.binary "image", limit: 4294967295
+    t.index ["id_category"], name: "fk_Producto_Categoria1_idx"
   end
 
-  create_table "usuario", primary_key: ["TipoId", "IdUsuario"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "TipoId", limit: 3, null: false
-    t.integer "IdUsuario", null: false
-    t.string "Nombre", limit: 70
-    t.string "Apellidos", limit: 45
-    t.integer "Telefono"
-    t.string "Direccion", limit: 50
-    t.string "CorreoElectronico", limit: 50
-    t.string "NombreUsuario", limit: 50
-    t.string "Contraseña", limit: 50
-    t.binary "Imagen", limit: 4294967295
-    t.string "Rol", limit: 10, comment: "Consumidor o Cultivador"
+  create_table "users", primary_key: ["id_person", "type_ID"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "Tabla maestra de personas" do |t|
+    t.integer "id_person", null: false, auto_increment: true
+    t.string "type_ID", limit: 3, null: false
+    t.string "first_name", limit: 70
+    t.string "last_name", limit: 45
+    t.integer "telephone"
+    t.string "address", limit: 50
+    t.string "email", limit: 50
+    t.string "username", limit: 50
+    t.string "password", limit: 50
+    t.binary "image", limit: 4294967295
+    t.string "role", limit: 10, comment: "Consumidor o Cultivador"
   end
 
-  add_foreign_key "calificacion", "pedido", column: "Pedido_idPedido", primary_key: "idPedido", name: "fk_Calificacion_Pedido1"
-  add_foreign_key "cliente", "usuario", column: "IdUsuario", primary_key: "IdUsuario", name: "fk_Consumidor_Usuario1"
-  add_foreign_key "cliente", "usuario", column: "TipoId", primary_key: "TipoId", name: "fk_Consumidor_Usuario1"
-  add_foreign_key "cuenta", "usuario", column: "IdUsuario", primary_key: "IdUsuario", name: "fk_Cuenta_Usuario1"
-  add_foreign_key "cuenta", "usuario", column: "TipoId", primary_key: "TipoId", name: "fk_Cuenta_Usuario1"
-  add_foreign_key "cultivador", "usuario", column: "IdUsuario", primary_key: "IdUsuario", name: "fk_Cultivador_Usuario1"
-  add_foreign_key "cultivador", "usuario", column: "TipoId", primary_key: "TipoId", name: "fk_Cultivador_Usuario1"
-  add_foreign_key "cultivador_has_producto", "cultivador", column: "Cultivador_IdUsuario", primary_key: "IdUsuario", name: "fk_Cultivador_has_Producto_Cultivador1"
-  add_foreign_key "cultivador_has_producto", "cultivador", column: "Cultivador_TipoId", primary_key: "TipoId", name: "fk_Cultivador_has_Producto_Cultivador1"
-  add_foreign_key "cultivador_has_producto", "producto", column: "Producto_ProductoId", primary_key: "ProductoId", name: "fk_Cultivador_has_Producto_Producto1"
-  add_foreign_key "despacho", "cultivador", column: "Cultivador_IdUsuario", primary_key: "IdUsuario", name: "fk_Despacho_Cultivador1"
-  add_foreign_key "despacho", "cultivador", column: "Cultivador_TipoId", primary_key: "TipoId", name: "fk_Despacho_Cultivador1"
-  add_foreign_key "despacho", "pedido", column: "Pedido_idPedido", primary_key: "idPedido", name: "fk_Despacho_Pedido1"
-  add_foreign_key "lineacompra", "pedido", column: "Pedido_idPedido", primary_key: "idPedido", name: "fk_LineaCompra_Pedido1"
-  add_foreign_key "lineacompra", "producto", column: "Producto_ProductoId", primary_key: "ProductoId", name: "fk_LineaCompra_Producto1"
-  add_foreign_key "pedido", "cliente", column: "Cliente_IdUsuario", primary_key: "IdUsuario", name: "fk_Pedido_Cliente1"
-  add_foreign_key "pedido", "cliente", column: "Cliente_TipoId", primary_key: "TipoId", name: "fk_Pedido_Cliente1"
-  add_foreign_key "producto", "categoria", column: "Categoria_CategoriaId", primary_key: "CategoriaId", name: "fk_Producto_Categoria1"
+  add_foreign_key "accounts", "users", column: "id_person", primary_key: "id_person", name: "fk_Account_Person1"
+  add_foreign_key "accounts", "users", column: "type_ID", primary_key: "type_ID", name: "fk_Account_Person1"
+  add_foreign_key "califications", "orders", column: "id_order", primary_key: "id_order", name: "fk_Calificacion_Pedido1"
+  add_foreign_key "clients", "users", column: "id_person", primary_key: "id_person", name: "fk_Client_Person1"
+  add_foreign_key "clients", "users", column: "type_ID", primary_key: "type_ID", name: "fk_Client_Person1"
+  add_foreign_key "cultivator_has_products", "cultivators", column: "id_person", primary_key: "id_person", name: "fk_Cultivator_has_Product_Cultivator1"
+  add_foreign_key "cultivator_has_products", "cultivators", column: "type_ID", primary_key: "type_ID", name: "fk_Cultivator_has_Product_Cultivator1"
+  add_foreign_key "cultivator_has_products", "products", column: "id_product", primary_key: "id_product", name: "fk_Cultivator_has_Product_Product1"
+  add_foreign_key "cultivators", "users", column: "id_person", primary_key: "id_person", name: "fk_Cultivator_Person1"
+  add_foreign_key "cultivators", "users", column: "type_ID", primary_key: "type_ID", name: "fk_Cultivator_Person1"
+  add_foreign_key "dispatches", "cultivators", column: "id_person", primary_key: "id_person", name: "fk_Dispatch_Cultivator1"
+  add_foreign_key "dispatches", "cultivators", column: "type_ID", primary_key: "type_ID", name: "fk_Dispatch_Cultivator1"
+  add_foreign_key "dispatches", "orders", column: "id_order", primary_key: "id_order", name: "fk_Despacho_Pedido1"
+  add_foreign_key "order_has_products", "orders", column: "id_order", primary_key: "id_order", name: "fk_LineaCompra_Pedido1"
+  add_foreign_key "order_has_products", "products", column: "id_product", primary_key: "id_product", name: "fk_LineaCompra_Producto1"
+  add_foreign_key "orders", "clients", column: "id_person", primary_key: "id_person", name: "fk_Order_Client1"
+  add_foreign_key "orders", "clients", column: "type_ID", primary_key: "type_ID", name: "fk_Order_Client1"
+  add_foreign_key "products", "categories", column: "id_category", primary_key: "id_category", name: "fk_Producto_Categoria1"
 end
